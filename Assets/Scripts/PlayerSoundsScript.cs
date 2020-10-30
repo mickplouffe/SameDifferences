fc4a0604dd4a9341901ced0a402749750eceb75a
Sulfide <chamacoselles@gmail.com>
2020-10-29 17:49:54 -0700

    Started working on Audio
    Do not run PlayerSoundsScript.cs yet because it seems to freeze the game engine at the moment. can someone find how to minimize it?
    
    I also moved two variables in PlayerController.cs outside a function so that I can use them in PlayerSoundsController.cs

---
+++ Assets/Scripts/PlayerSoundsScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundsScript : MonoBehaviour
+{
+    public AudioSource Feet;
+    public AudioSource SoundPlayer;
+    public CharacterController Character;
+    public PlayerController Controller;
+
+    public AudioSource[] Talking;
+    public AudioSource[] Pickup;
+    public AudioSource[] Footsteps;
+
+    private float WalkingStepTime;
+    
+    public IEnumerator FootStepsPlayer() {
+        float WalkingSpeed = Controller.walkingSpeed;
+
+        while (true) {
+            float CurrentSpeed = Controller.curSpeedX + Controller.curSpeedY;
+
+            if (Character.isGrounded) {
+                if (CurrentSpeed != 0) {
+                    if (CurrentSpeed <= WalkingSpeed) {
+                        WalkingStepTime = 0.7f;
+                    } else {
+                        WalkingStepTime = 0.5f;
+                    }
+
+                    Debug.Log("Insert Footstep Sound");
+                    yield return new WaitForSeconds(WalkingStepTime);
+                }
+            }
+        }
+    }
+
+
+    void Start() {
+        StartCoroutine("FootStepsPlayer");
+    }
+}
